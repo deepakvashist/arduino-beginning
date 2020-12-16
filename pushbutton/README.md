70802d65494545e2474def9b471a21cb80d297c7
Deepak Vashist <deepak@Deepaks-MacBook-Pro.local>
2020-12-15 23:36:34 -0300

    Add pushbutton project

---
+++ pushbutton/README.md
@@ -0,0 +1,3 @@
+# External LED
+
+![Project](../images/pushbutton.jpg)
---
+++ pushbutton/pushbutton.ino
@@ -0,0 +1,19 @@
+int led = 2;
+int button = 3;
+int isButtonActive = 0;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
+}
+
+void loop() {
+  isButtonActive = digitalRead(button);
+  
+  if(isButtonActive == HIGH) {
+    digitalWrite(led, HIGH);
+    delay(5000);
+  } else {
+    digitalWrite(led, LOW);
+  }
+}
